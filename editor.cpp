17c58580131cca82ebb733bb19bacd02981bf8ea
Michael Eggers <mseggers@protonmail.ch>
2024-01-05 22:42:20 +0100

    more documentation. Maybe MacOS fix

--- editor.cpp
+++ editor.cpp
@@ -4,7 +4,6 @@
 
 #include <vector>
 #include <algorithm>
-#include <thread>
 
 #include <glad/glad.h>
 
--- processor.h
+++ processor.h
@@ -14,6 +14,7 @@
 class Processor : public IEventSubscriber {
 public:
 	Processor(EventHandler* eventHandler);
+	Processor(Processor &&) = default;
 	virtual ~Processor();
 
 	virtual void Update(IEvent* event) override;
